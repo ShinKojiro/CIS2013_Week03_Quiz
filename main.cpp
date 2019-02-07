78542fe807ba1693abd87c22b624e7f754965db9
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-06 18:16:27 -0600

    even MORE logic

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@ int main(){
     int i = 99;
 
     for(i = 99; i >= 0; i--){
-        if(i > 0){
+        if (i == 1){
+            cout << i << " bottle of beer on the wall, " << i << " bottle of beer," << endl
+            << "Take one down and pass it around, " << (i - 1) << " bottles of beer on the wall.\n" << endl;
+        }
+        else if(i == 2){
+            cout << i << " bottles of beer on the wall, " << i << " bottles of beer," << endl
+            << "Take one down and pass it around, " << (i - 1) << " bottle of beer on the wall.\n" << endl;
+        }
+        else if(i > 0){
             cout << i << " bottles of beer on the wall, " << i << " bottles of beer," << endl
             << "Take one down and pass it around, " << (i - 1) << " bottles of beer on the wall.\n" << endl;
         }
