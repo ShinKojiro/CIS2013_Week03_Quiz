1a9df4fc54000cc6bd0b6686b4f4367e7cbe3b50
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-06 18:11:47 -0600

    added some LOGIC

--- main.cpp
+++ main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 int main(){
     int i = 99;
 
-    for(i = 99; i > 0; i--){
-        cout << i << " bottles of beer on the wall, " << i << " bottles of beer," << endl
-        << "Take one down and pass it around, " << (i - 1) << " bottles of beer on the wall.\n" << endl;
+    for(i = 99; i >= 0; i--){
+        if(i > 0){
+            cout << i << " bottles of beer on the wall, " << i << " bottles of beer," << endl
+            << "Take one down and pass it around, " << (i - 1) << " bottles of beer on the wall.\n" << endl;
+        }
+        else if(i == 0){
+            cout << "No more bottles of beer on the wall, no more bottles of beer," << endl
+            << "Go to the store and buy some more, " << i + 99 << " bottles of beer on the wall.\n" << endl;
+        }
+        
     }
 
     
